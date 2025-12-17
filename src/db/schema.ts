4d200ce09870c718ea48174ff0b10fb2a65c9d27
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2025-12-17 19:24:52 +0530

    feat: Schema tweak

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
+// TODO: Do not null
 export const MessagesTable = pgTable("messages", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   message: varchar().notNull(),
   link: varchar().notNull().unique(),
+  currentViews: integer().default(0),
+  maxViews: integer().notNull(),
+  iv: varchar().notNull(),
+  expiresAt: timestamp(),
+  createdAt: timestamp().defaultNow(),
 });
